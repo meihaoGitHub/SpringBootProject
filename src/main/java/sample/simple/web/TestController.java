7c2aeead0af8e7d25164cd0e9eab730796937abb
ko.bai <ko_bai@cm.jip.co.jp>
2015-11-30 17:25:45 +0900

    2015.11.30 開発環境テスト

--- src/main/java/sample/simple/web/TestController.java
+++ src/main/java/sample/simple/web/TestController.java
@@ -1,6 +1,7 @@
 package sample.simple.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,7 +16,15 @@ public class TestController {
     }
 
     @RequestMapping(value = "/thymeleaf-sample")
-    public String thymeleafSample() {
+
+    public String thymeleafSample(Model model) {
+
+        model.addAttribute("msg", "引数 model を追加してみました。");
+        User user = new User();
+        user.setName("田中　太郎");
+        user.setTel("03-1234-5678");
+        model.addAttribute("user", user);
+
         return "test/thymeleaf-sample2";
     }
 
