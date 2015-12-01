75f4c2bfcd0dfec4f7bfb4a414c779b8c88cc1eb
meihao <meihaohpoto@gmail.com>
2015-12-01 21:45:48 +0900

    2015.12.01 ログ出力追加

--- src/main/java/sample/simple/web/TestController.java
+++ src/main/java/sample/simple/web/TestController.java
@@ -1,5 +1,7 @@
 package sample.simple.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/test")
 public class TestController {
+
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @RequestMapping
     @ResponseBody
@@ -18,6 +22,7 @@ public class TestController {
     @RequestMapping(value = "/thymeleaf-sample")
     public String thymeleafSample(Model model) {
 
+        logger.info("thymeleafSample を実行");
         model.addAttribute("msg", "引数 model を追加してみました。");
         User user = new User();
         user.setName("田中　太郎");
