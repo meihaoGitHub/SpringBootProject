28152d3ed908d867fd66e5188240c3b2f21d7d03
ko.bai <ko_bai@cm.jip.co.jp>
2015-12-04 17:10:57 +0900

    2015.12.04 開発環境テスト ログを追加

--- src/main/java/sample/simple/web/TestController.java
+++ src/main/java/sample/simple/web/TestController.java
@@ -1,11 +1,11 @@
 package sample.simple.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+        import org.slf4j.Logger;
+        import org.slf4j.LoggerFactory;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/test")
